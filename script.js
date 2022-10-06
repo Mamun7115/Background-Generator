3c0a12049c9f46f48847b4bf9c9fb4e9b1254652
lustofthelost <mahmed7115@gmail.com>
2022-10-06 16:31:23 +0100

    Added loadash and live-server

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6,7,8]
+console.log("answer:",_.without(array,3))
 
 const body = document.querySelector('body')
 const first = document.querySelector('.first').value;
